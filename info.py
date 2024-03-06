b33f38a576e781182d3f0c23110f262fd0945902
TN69Links-Saravana <157959005+TN69Links-Saravana@users.noreply.github.com>
2024-03-06 23:29:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,14 +48,14 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
+DATABASE_URI = environ.get('DATABASE_URI', "ongodb+srv://SaravanaPerumal:JTTjvAcCAuorrdiA@saravana-perumal.gnmp50t.mongodb.net/?retryWrites=true&w=majority&appName=Saravana-Perumal")
+DATABASE_NAME = environ.get('DATABASE_NAME', "SaravanaPerumal")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'moneykamalo.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdisk.info')
+SHORTLINK_API = environ.get('SHORTLINK_API', '16d0ccbd4d81fd7a48c84da1ba1336ad0546680e')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
